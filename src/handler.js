2084b23a77036c7cf2d7d1fdd4ab7c0830428a86
Rizqi <hidayatullah560@gmail.com>
2022-07-08 22:06:28 +0800

    fix bugs

--- src/handler.js
+++ src/handler.js
@@ -31,7 +31,7 @@ const addNoteHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    massage: 'Catatan gagal ditambahkan',
+    message: 'Catatan gagal ditambahkan',
   });
   response.code(500);
   return response;
@@ -60,7 +60,7 @@ const getNoteByIdHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    massage: 'Catatan tidak ditemukan',
+    message: 'Catatan tidak ditemukan',
   });
   response.code(404);
   return response;
@@ -85,7 +85,7 @@ const editNoteByIdHandler = (request, h) => {
 
     const response = h.response({
       status: 'success',
-      massage: 'Catatan berhasil diperbarui',
+      message: 'Catatan berhasil diperbarui',
     });
     response.code(200);
     return response;
@@ -93,7 +93,7 @@ const editNoteByIdHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    massage: 'Gagal memperbarui catatan. Id tidak ditemukan',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
   });
   response.code(404);
   return response;
@@ -108,7 +108,7 @@ const deleteNoteByIdHandler = (request, h) => {
     notes.splice(index, 1);
     const response = h.response({
       status: 'success',
-      massage: 'Catatan berhasil dihapus',
+      message: 'Catatan berhasil dihapus',
     });
     response.code(200);
     return response;
@@ -116,7 +116,7 @@ const deleteNoteByIdHandler = (request, h) => {
 
   const response = h.response({
     status: 'fail',
-    massage: 'Catatan gagal dihapus. Id tidak ditemukan',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
   });
   response.code(404);
   return response;
